8abe7f6b4ebd5f08215012e9bcef736f7dd6925c
ajaas <ajaasazeez@gmail.com>
2021-08-21 14:56:25 +0400

    location manager added

--- app/src/main/java/com/example/tweetmap/ui/fragments/TweetListFragment.kt
+++ app/src/main/java/com/example/tweetmap/ui/fragments/TweetListFragment.kt
@@ -1,33 +1,59 @@
 package com.example.tweetmap.ui.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import com.example.tweetmap.R
 import com.example.tweetmap.data.Resource
 import com.example.tweetmap.databinding.FragmentTweetListBinding
-import com.example.tweetmap.utils.PrefsHelper
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class TweetListFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+@AndroidEntryPoint
+class TweetListFragment : Fragment() {
+    private lateinit var googleMap: GoogleMap
     private lateinit var binding: FragmentTweetListBinding
     private val tweetListViewModel: TweetListViewModel by viewModels()
+    private lateinit var locationManager: LocationManager
+    private val locationListener = LocationListener {
+        moveMapToCurrentLocation(it)
+        /*googleMap.addMarker(
+                MarkerOptions().position(currentLocation).title("Marker Title").snippet("Marker Description")
+            )*/
+    }
+
+    @SuppressLint("MissingPermission")
+    private val requestPermissions =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                googleMap.isMyLocationEnabled = true
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 90 * 1000,
+                    10.0.toFloat(), locationListener
+                )
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -40,7 +66,9 @@ class TweetListFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.map.onCreate(savedInstanceState)
-        binding.map.onResume()
+        locationManager =
+            activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        setUpGoogleMap()
         tweetListViewModel.getToken()
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
             androidx.appcompat.widget.SearchView.OnQueryTextListener {
@@ -60,28 +88,52 @@ class TweetListFragment : Fragment(), OnMapReadyCallback {
 
     }
 
+    private fun setUpGoogleMap() {
+        binding.map.onResume()
+        try {
+            MapsInitializer.initialize(requireActivity().applicationContext)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        binding.map.getMapAsync { mMap ->
+            googleMap = mMap
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+            } else {
+                googleMap.isMyLocationEnabled = true
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 90 * 1000,
+                    10.0.toFloat(), locationListener
+                )
+            }
+        }
+    }
+
     private fun setObservers() {
         tweetListViewModel.streamResponseLiveData.observe(viewLifecycleOwner, Observer {
             when (it) {
-                is Resource.Loading -> Log.e("Loading","true") /*showLoadingView()*/
-                is Resource.Success -> Log.e("Success",Gson().toJson(it.data))
+                is Resource.Loading -> Log.e("Loading", "true") /*showLoadingView()*/
+                is Resource.Success -> Log.e("Success", Gson().toJson(it.data))
                 is Resource.DataError -> {
-                   /* showDataView(false)
-                    status.errorCode?.let { recipesListViewModel.showToastMessage(it) }*/
+                    /* showDataView(false)
+                     status.errorCode?.let { recipesListViewModel.showToastMessage(it) }*/
                 }
             }
         })
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(
-            MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun moveMapToCurrentLocation(location: Location) {
+        val currentLocation = LatLng(location.latitude, location.longitude)
+        val cameraPosition = CameraPosition.Builder().target(currentLocation).zoom(12f).build()
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
+
+
 }
